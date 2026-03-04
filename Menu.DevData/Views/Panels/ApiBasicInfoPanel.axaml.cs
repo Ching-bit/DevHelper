8123b07eba5a282b6137c938d94ac8ebb7674083
QingyunWu <wuhaiwqy@hotmail.com>
2026-03-04 18:15:56 +0800

    api basic info panel

---
+++ Menu.DevData/Views/Panels/ApiBasicInfoPanel.axaml.cs
@@ -0,0 +1,40 @@
+using Attributes.Avalonia;
+using Avalonia;
+using Framework.Common;
+
+namespace Menu.DevData;
+
+[WithDirectProperty(typeof(string), "ApiName", nullable: true)]
+[WithDirectProperty(typeof(string), "ApiDescription", nullable: true)]
+[WithDirectProperty(typeof(string), "Remark", nullable: true)]
+[WithDirectProperty(typeof(bool), "IsBasicInfoChanged")]
+[WithDirectProperty(typeof(ModifyStatus), "InternalRemarkModifyStatus")]
+public partial class ApiBasicInfoPanel : UniPanel
+{
+    public ApiBasicInfoPanel()
+    {
+        InitializeComponent();
+    }
+    
+    private string? _remarkOriginalValue;
+    
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs e)
+    {
+        base.OnPropertyChanged(e);
+
+        if (e.Property == RemarkProperty)
+        {
+            InternalRemarkModifyStatus =
+                _remarkOriginalValue == Remark ? ModifyStatus.Normal : ModifyStatus.Modified;
+            IsBasicInfoChanged = InternalRemarkModifyStatus != ModifyStatus.Normal;
+        }
+        else if (e.Property == IsBasicInfoChangedProperty)
+        {
+            if (!IsBasicInfoChanged)
+            {
+                _remarkOriginalValue = Remark;
+                InternalRemarkModifyStatus = ModifyStatus.Normal;
+            }
+        }
+    }
+}
